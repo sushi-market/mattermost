3392054c9bc71dd8aeb2341a4f9a2184fba33386
denis660 <79048203075@ya.ru>
2026-04-27 15:44:30 +0600

    custom: translate team menu and channel header

--- webapp/channels/src/components/sidebar/sidebar_header/sidebar_team_menu.test.tsx
+++ webapp/channels/src/components/sidebar/sidebar_header/sidebar_team_menu.test.tsx
@@ -121,7 +121,7 @@ describe('components/sidebar/sidebar_header/sidebar_team_menu', () => {
             expect(screen.getByText('Manage members')).toBeInTheDocument();
             expect(screen.getByText('Leave team')).toBeInTheDocument();
             expect(screen.getByText('Create a team')).toBeInTheDocument();
-            expect(screen.getByText('Learn about teams')).toBeInTheDocument();
+            expect(screen.queryByText('Learn about teams')).not.toBeInTheDocument();
         });
     });
 
@@ -293,4 +293,3 @@ describe('components/sidebar/sidebar_header/sidebar_team_menu', () => {
         });
     });
 });
-
--- webapp/channels/src/components/sidebar/sidebar_header/sidebar_team_menu.tsx
+++ webapp/channels/src/components/sidebar/sidebar_header/sidebar_team_menu.tsx
@@ -7,7 +7,6 @@ import {useDispatch, useSelector} from 'react-redux';
 import {useHistory} from 'react-router-dom';
 
 import {
-    LightbulbOutlineIcon,
     AccountPlusOutlineIcon,
     AccountMultiplePlusOutlineIcon,
     SettingsOutlineIcon,
@@ -114,8 +113,6 @@ export default function SidebarTeamMenu(props: Props) {
                     isCloud={isCloud}
                 />
             )}
-            <Menu.Separator/>
-            <LearnAboutTeamsMenuItem/>
             <PluginMenuItems/>
         </Menu.Container>
     );
@@ -453,33 +450,6 @@ function RestrictedIndicatorForCreateTeam({isFreeTrial}: {isFreeTrial: boolean})
                 id: 'navbar_dropdown.create.modal.messageEndUser',
                 defaultMessage: 'Multiple teams allow for context-specific spaces that are more attuned to your teams’ needs.',
             })}
-        />
-    );
-}
-
-const MATTERMOST_ACADEMY_TEAM_TRAINING_LINK = 'https://mattermost.com/pl/mattermost-academy-team-training';
-
-function LearnAboutTeamsMenuItem() {
-    const handleClick = useCallback(() => {
-        window.open(MATTERMOST_ACADEMY_TEAM_TRAINING_LINK, '_blank', 'noopener noreferrer');
-    }, []);
-
-    return (
-        <Menu.Item
-            className='learnAboutTeamsMenuItem'
-            onClick={handleClick}
-            leadingElement={(
-                <LightbulbOutlineIcon
-                    size={18}
-                    aria-hidden='true'
-                />
-            )}
-            labels={(
-                <FormattedMessage
-                    id='sidebarLeft.teamMenu.learnAboutTeamsMenuItem.primaryLabel'
-                    defaultMessage='Learn about teams'
-                />
-            )}
         />
     );
 }
